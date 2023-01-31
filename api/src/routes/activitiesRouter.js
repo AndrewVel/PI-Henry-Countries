6f5bb99a2458c3721464234222fddf855f738b3a
Andrew <andres.velata@hotmail.com>
2023-01-31 00:42:41 -0500

    Listo Final

--- api/src/routes/activitiesRouter.js
+++ api/src/routes/activitiesRouter.js
@@ -19,7 +19,7 @@ activitiesRouter.post("/", async (req, res) => {
     );
     res.status(200).json(activity);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(404).json({ error: error.message });
   }
 });
 
