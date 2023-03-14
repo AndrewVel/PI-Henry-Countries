abb888b6423143a1bbc06ceca935c071118c56d3
Andrew <andres.velata@hotmail.com>
2023-03-14 00:01:07 -0500

    cv y foto

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -13,7 +13,7 @@ const Me = () => {
           <div className="about-img">
             <a href="https://www.linkedin.com/in/andres-velata/">
               <img
-                src="https://scontent.fltx1-1.fna.fbcdn.net/v/t1.6435-9/74387942_3170784979605012_3312521409967685632_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=cIIWt__vTmUAX-7CIbm&tn=OlQt3g5CzP4zS1OJ&_nc_ht=scontent.fltx1-1.fna&oh=00_AfDP-j1QWeY4cU9CTLFtsA1o_cxSk-t3b-8N1kJlO0CRJQ&oe=6402CF6B"
+                src={andrew}
                 alt="Andrew"
               />
             </a>
